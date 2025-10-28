4ec338d7a376ebdd8b2aa309332887da62554411
Tommy Odland <todl@equinor.com>
2025-10-28 14:43:35 +0100

    Better error message if PDM returns zero rows

--- dca/adca/dataloaders.py
+++ dca/adca/dataloaders.py
@@ -294,14 +294,19 @@ def load_PDM_data(
     from pdm_datareader import tools  # Only import as needed
 
     log.info("Reading Production Data Mart (PDM).")
-    df = tools.query(sql).assign(
+    df = tools.query(sql)
+    if df.empty:
+        msg = "Connection to PDM was successful, but zero rows were returned from the database.\n"
+        msg += "Please check your well IDs and make sure they match well IDs that exist in PDM."
+        raise Exception(msg)
+
+    df = df.assign(
         well_id=lambda df: df.wb_uwbi,
         production=lambda df: pd.to_numeric(df[phases].sum(axis=1)),
         time=lambda df: df.prod_day.dt.to_period("D"),
         time_on=lambda df: pd.to_numeric(df.time_on),
     )
     log.info(f"Read {len(df)} rows from '{table}'")
-    assert not df.empty
 
     # Print data quality report
     log.info("\nPDM data quality report (raw data, detailed)")
