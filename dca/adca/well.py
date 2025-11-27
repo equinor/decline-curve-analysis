c5152a5f4817d801c41f765003d50d6f785725e7
Knut Utne Hollund <kuho@equinor.com>
2025-11-27 11:27:12 +0100

    remove locals

--- dca/adca/well.py
+++ dca/adca/well.py
@@ -499,6 +499,8 @@ class Well:
 
         # Plot the DCA curve
         x_labels_all = self.time.values
+        y_axis_min = None
+        y_axis_max = None
         if prediction and self.is_fitted():
             # Set up the future grid
             freq = self.time.dtype.freq.freqstr
@@ -532,8 +534,8 @@ class Well:
             # The expected curve
             y_smooth = self.predict(x_smooth, time_on=w_smooth, q=None)
             y_smooth = np.log(y_smooth) if logscale else y_smooth
-            y_max = np.max(y_smooth)
-            y_min = np.min(y_smooth)
+            y_axis_max = np.max(y_smooth)
+            y_axis_min = np.min(y_smooth)
             ax.plot(
                 x_smooth,
                 y_smooth,
@@ -546,8 +548,8 @@ class Well:
             for q_i in q:
                 y_smooth = self.predict(x_smooth, time_on=w_smooth, q=q_i)
                 y_smooth = np.log(y_smooth) if logscale else y_smooth
-                y_max = max(y_max, np.max(y_smooth))
-                y_min = min(y_min, np.min(y_smooth))
+                y_axis_max = max(y_axis_max, np.max(y_smooth))
+                y_axis_min = min(y_axis_min, np.min(y_smooth))
                 ax.plot(
                     x_smooth,
                     y_smooth,
@@ -576,10 +578,11 @@ class Well:
         # Attempt to set up a sensible y-axis
         _, y, w = self.get_curve_data()
         y = np.log(y[w > 0]) if logscale else y[w > 0]
-        y_max = max(y_max, np.max(y)) if "y_max" in locals() else np.max(y)
-        y_min = min(y_min, np.min(y)) if "y_min" in locals() else np.min(y)
-        y_range = y_max - y_min
-        ax.set_ylim(y_min - y_range / 10, y_max + y_range / 10)
+        y_axis_max = max(y_axis_max, np.max(y)) if y_axis_max else np.max(y)
+        y_axis_min = min(y_axis_min, np.min(y)) if y_axis_min else np.min(y)
+        y_axis_range = y_axis_max - y_axis_min
+        ax.set_ylim(y_axis_min - y_axis_range / 10, y_axis_max + y_axis_range / 10)
+
         ax.set_ylabel(y_label)
 
         # Plot dates along x axis
