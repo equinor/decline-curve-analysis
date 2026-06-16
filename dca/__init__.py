27e93449d136b3d50cb428b3cae585bd9228f71f
Tommy Odland <10076072+tommyod@users.noreply.github.com>
2026-06-16 09:48:59 +0200

    Remove contact info (#84)
    Remove todl contact info

--- dca/__init__.py
+++ dca/__init__.py
@@ -2,10 +2,6 @@
 This file contains Python code related to Decline Curve Analysis (DCA).
 The latest version can be found at:
     https://github.com/equinor/decline-curve-analysis
-
-The code was written by:
-    Knut Utne Hollund <kuho@equinor.com>
-    Tommy Odland <todl@equinor.com>
 """
 
 from dca.decline_curve_analysis import Arps, CurveLoss, Exponential
--- dca/adca/__init__.py
+++ dca/adca/__init__.py
@@ -2,10 +2,6 @@
 This file contains Python code related to Decline Curve Analysis (DCA).
 The latest version can be found at:
     https://github.com/equinor/decline-curve-analysis
-
-The code was written by:
-    Knut Utne Hollund <kuho@equinor.com>
-    Tommy Odland <todl@equinor.com>
 """
 
 from dca.adca.utils import clean_well_data
